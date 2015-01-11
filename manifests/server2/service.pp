a7460728bb257bdca66030e9e6b6e8771fa9f687
František Dvořák <valtri@civ.zcu.cz>
2015-01-11 13:31:43 +0100

    Dependency of server2 service on metastore (not strictly required but let's not throw exceptions to the logs).

--- manifests/server2/service.pp
+++ manifests/server2/service.pp
@@ -6,4 +6,12 @@ class hive::server2::service {
     enable    => true,
     subscribe => [File['hive-site.xml']],
   }
+
+  # launch metastore first if collocated with server2
+  # (dependency is not strictly required though, server2 can wait for
+  # metastore, but let's not throw exceptions to logs)
+  if $hive::metastore_hostname == $::fqdn {
+    include hive::metastore::service
+    Class['hive::metastore::service'] -> Class['hive::server2::service']
+  }
 }
