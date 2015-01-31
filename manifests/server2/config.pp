a16c3f227711843f3aab45adb6bf95c6a324de91
František Dvořák <valtri@civ.zcu.cz>
2015-02-01 00:53:53 +0100

    Configuration of Hadoop YARN and MapReduce is required for server2 (for example JDBC connection with security).

--- manifests/server2/config.pp
+++ manifests/server2/config.pp
@@ -3,6 +3,8 @@
 # Configuration of Hive server2 service.
 #
 class hive::server2::config {
+  contain hadoop::common::yarn::config
+  contain hadoop::common::mapred::config
   contain hive::common::config
   contain hive::common::daemon
 }
