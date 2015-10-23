316704fc7ff994bbae8bebf60aba26f76576e771
František Dvořák <valtri@civ.zcu.cz>
2015-10-23 16:43:54 +0200

    Using shared hadoop_lib module for picking JDBC jar files.

--- manifests/metastore/config.pp
+++ manifests/metastore/config.pp
@@ -6,19 +6,7 @@ class hive::metastore::config {
   contain hive::common::config
   contain hive::common::daemon
 
-  if $hive::db == 'mysql' or $hive::db == 'mariadb' {
-    file { '/usr/lib/hive/lib/mysql-connector-java.jar':
-      ensure => 'link',
-      links  => 'follow',
-      source => '/usr/share/java/mysql-connector-java.jar',
-    }
-  }
-
-  if $hive::db == 'postgresql' {
-    file { '/usr/lib/hive/lib/postgresql-jdbc.jar':
-      ensure => 'link',
-      links  => 'follow',
-      source => '/usr/share/java/postgresql.jar',
-    }
+  hadoop_lib::jdbc { '/usr/lib/hive/lib':
+    db => $::hive::db,
   }
 }
